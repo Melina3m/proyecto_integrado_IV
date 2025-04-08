e133ade2b4786d1927d0c3a4fb0d91b8f3aaa108
Melina3m <melina.munoz@est.iudigital.edu.co>
2025-04-07 21:49:37 -0500

    correcciones querys

--- queries/top_10_revenue_categories.sql
+++ queries/top_10_revenue_categories.sql
@@ -1,27 +1,19 @@
--- TODO: Esta consulta devolverá una tabla con las 10 categorías con mayores ingresos
--- (en inglés), el número de pedidos y sus ingresos totales. La primera columna será
--- Category, que contendrá las 10 categorías con mayores ingresos; la segunda será
--- Num_order, con el total de pedidos de cada categoría; y la última será Revenue,
--- con el ingreso total de cada categoría.
--- PISTA: Todos los pedidos deben tener un estado 'delivered' y tanto la categoría
--- como la fecha real de entrega no deben ser nulas.
 SELECT
-    pct.product_category_name_english AS Category,
-    COUNT(DISTINCT o.order_id) AS Num_order,
-    SUM(oi.price + oi.freight_value) AS Revenue
-FROM olist_orders AS o
-JOIN olist_order_items AS oi 
-    ON o.order_id = oi.order_id
-JOIN olist_products AS p
-    ON oi.product_id = p.product_id
-JOIN product_category_name_translation AS pct 
-    ON p.product_category_name = pct.product_category_name
-WHERE
-    o.order_status = 'delivered' 
-    AND o.order_delivered_customer_date IS NOT NULL 
-    AND p.product_category_name IS NOT NULL
-GROUP BY
-    pct.product_category_name_english
-ORDER BY
-    Revenue DESC -- Corregido aquí
-LIMIT 10; -- Corregido aquí
+    pcnt.product_category_name_english AS Category,
+    COUNT(DISTINCT ooi.order_id) AS Num_order,
+    SUM(oop.payment_value) AS Revenue
+FROM olist_order_items ooi
+JOIN olist_products op 
+    ON op.product_id = ooi.product_id
+JOIN product_category_name_translation pcnt 
+    ON op.product_category_name = pcnt.product_category_name
+JOIN olist_orders oo 
+    ON ooi.order_id = oo.order_id
+JOIN olist_order_payments oop 
+    ON oo.order_id = oop.order_id
+WHERE oo.order_status = 'delivered'
+  AND oo.order_delivered_customer_date IS NOT NULL
+  AND pcnt.product_category_name IS NOT NULL
+GROUP BY Category
+ORDER BY Revenue DESC
+LIMIT 10;
